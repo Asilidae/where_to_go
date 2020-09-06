7384b93ff02451e769eb02bdee9f2b8d8854e1cd
Asilidae <manhin@yandex.ru>
2020-09-06 22:19:21 +0300

    Displaying photo previews

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Place, Image
 # Register your models here.
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ["get_preview"]
+    fields = ('image', 'get_preview', 'number',)
+
+    def get_preview(self, obj):
+        return mark_safe(
+            '<img src="{url}" height={height} />'.format(
+                url=obj.image.url,
+                height='200px',
+            )
+        )
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
